133f237e8abec7fa18f081962421310c4439cfd7
Jordi Moraleda <moraleda@uniclau.com>
2017-11-28 18:46:36 +0100

    WIP

--- components/top-sale-status.js
+++ components/top-sale-status.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import Web3 from "web3";
 import { HashStore } from "../assets/contracts.js";
 
 const tokenSaleAddress = "0x8af4943ED2744c229976D94045854dc5e374479a";
@@ -6,10 +7,16 @@ const tokenSaleAddress = "0x8af4943ED2744c229976D94045854dc5e374479a";
 export default class extends React.Component {
 	state = {
 		status: "(Please wait)",
-		unsupported: false
+		loading: true
 	};
 
 	componentDidMount() {
+		// window.w3 = new Web3(web3.currentProvider);
+		// window.HashStore = HashStore;
+		// window.w3 = new Web3("http://localhost:8545");
+		// Contract = new w3.eth.Contract(HashStore.abi, "0x8af4943ED2744c229976D94045854dc5e374479a")
+		// Contract.deploy({data: HashStore.byteCode, arguments: ["0x1234"]}).send({from: "0xE18D8EB2b5d0d141908A7eBF672DC77D8681902b"})
+
 		this.HashStoreContract = ethTx.wrapContract(HashStore.abi, HashStore.byteCode);
 
 		ethTx.onConnectionChanged(status => this.connectionChanged(status));
@@ -22,7 +29,7 @@ export default class extends React.Component {
 
 			return ethTx.getNetwork();
 		}).then(name => {
-			this.setState({ connected: true, network: name });
+			this.setState({ connected: true, network: name, loading: false });
 
 			if (name != "ropsten")
 				throw new Error("Please, switch to the Ropsten network");
@@ -30,7 +37,7 @@ export default class extends React.Component {
 			this.updateInterval = setInterval(() => this.updateSaleStatus(), 3000);
 			return this.updateSaleStatus();
 		}).catch(err => {
-			this.setState({ status: err.message });
+			this.setState({ status: err.message, loading: false });
 		});
 	}
 
@@ -76,6 +83,8 @@ export default class extends React.Component {
 	}
 
 	connectionChanged(status) {
+		this.setState(status);
+
 		if (!status.connected)
 			this.setState({
 				status:
@@ -88,7 +97,7 @@ export default class extends React.Component {
 
 	submit() {
 		this.attachToContract();
-debugger;
+		debugger;
 		const hash = "0x1234";
 
 		return ethTx.getNetwork()
@@ -100,7 +109,7 @@ debugger;
 			})
 			.then(result => {
 				console.log(result);
-				this.setState({status: "Updated the hash to " + hash});
+				this.setState({ status: "Updated the hash to " + hash });
 
 				return updateStatus();
 			})
@@ -115,6 +124,7 @@ debugger;
 			<style jsx>{`
 				#web3-form {
 					padding: 20px 0;
+					background-color: #ecfefb;
 				}
 
 				.text-center.text-muted {
@@ -125,7 +135,7 @@ debugger;
 				}
 			`}</style>
 
-			<div className="col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-sm-10 offset-sm-1">
+			<div className="col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-sm-10 offset-sm-1 text-center">
 				<label>Token Sale address</label>
 				<div className="input-group">
 					<span className="input-group-addon">@</span>
@@ -142,7 +152,7 @@ debugger;
 						<button className="btn btn-outline-info" type="button"><strong>$</strong> &darr;</button>
 					</span>
 					<span className="input-group-btn">
-						<button className="btn btn-success" type="button" onClick={() => this.submit() }>Fund the project</button>
+						<button className="btn btn-success" type="button" onClick={() => this.submit()}>Fund the project</button>
 					</span>
 				</div>
 				<p className="text-center text-muted">234.00 USD - 1.0234 ETH - 1234 Tokens</p>
@@ -153,11 +163,90 @@ debugger;
 	}
 
 	renderChromeReady() {
+		return <div id="web3-ready" className="row">
+			<style jsx>{`
+				#web3-ready {
+					padding: 20px 0;
+					background-color: #fcfeec;
+				}
 
+				.btn {
+					cursor: pointer;
+				}
+			`}</style>
+
+			<div className="col-lg-6 offset-lg-3 col-md-8 offset-md-2 col-sm-10 offset-sm-1 text-center">
+				<label>Token Sale address</label>
+				<div className="input-group">
+					<span className="input-group-addon">@</span>
+					<input type="text" readOnly={true} className="form-control" aria-label="Token Sale Address" value={tokenSaleAddress} />
+				</div>
+
+				{/* <p className="text-center text-muted">You currently have a balance of 100 Tokens</p> */}
+
+				<hr />
+				<h6>Support for Ethereum</h6>
+				<p>In order to launch your transaction, please, check one of these options:</p>
+
+				<div className="row text-center">
+					<div className="col-6">
+						<a href="" className="btn btn-block btn-outline-primary">Install Metamask<br />for Chrome</a>
+						<p><small><a href="#" className="text-muted">More info</a></small></p>
+					</div>
+					<div className="col-6">
+						<a href="" className="btn btn-block btn-outline-success">Connect with <br />MyEtherwallet</a>
+						<p><small><a href="#" className="text-muted">More info</a></small></p>
+					</div>
+				</div>
+			</div>
+		</div>
 	}
 
 	renderNoWeb3() {
+		return <div id="web3-missing" className="row">
+			<style jsx>{`
+				#web3-missing {
+					padding: 20px 0;
+					background-color: #fef9ec
+				}
+			`}</style>
 
+			<div className="col-lg-8 offset-lg-2 col-md-10 offset-md-1 text-center">
+				<h6>Your browser is not compatible with Ethereum</h6>
+				<p>Please, check one of these options:</p>
+
+				<div className="row text-center">
+					<div className="col-md-3 col-sm-6">
+						<a href="#"><img src="http://via.placeholder.com/80x80" className="rounded-circle" /></a>
+						<h6>Chrome + Metamask</h6>
+						<p><small><a href="#" className="text-muted">More info</a></small></p>
+					</div>
+					<div className="col-md-3 col-sm-6">
+						<a href="#"><img src="http://via.placeholder.com/80x80" className="rounded-circle" /></a>
+						<h6>Ethereum Mist</h6>
+						<p><small><a href="#" className="text-muted">More info</a></small></p>
+					</div>
+					<div className="col-md-3 col-sm-6">
+						<a href="#"><img src="http://via.placeholder.com/80x80" className="rounded-circle" /></a>
+						<h6>Parity</h6>
+						<p><small><a href="#" className="text-muted">More info</a></small></p>
+					</div>
+					<div className="col-md-3 col-sm-6">
+						<a href="#"><img src="http://via.placeholder.com/80x80" className="rounded-circle" /></a>
+						<h6>MyEtherWallet</h6>
+						<p><small><a href="#" className="text-muted">More info</a></small></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	}
+
+	fundingSection(){
+		if(this.state.loading) return <div/>;
+		else if(!this.state.connected) return this.renderNoWeb3();
+		else if(window && !!window.chrome) return this.renderWeb3Ready()
+		// else if(!this.state.accounts || !this.state.accounts.length) return this.renderWeb3Ready()
+		else return this.renderWeb3Ready()
 	}
 
 	render() {
@@ -194,10 +283,9 @@ debugger;
 						</div>
 					</div>
 
-					{this.renderWeb3Ready()}
+					{/* <p className="text-center text-muted">{this.state.status}</p> */}
 
-
-					<p className="text-center text-muted">{this.state.status}</p>
+					{this.fundingSection()}
 
 				</div>
 
