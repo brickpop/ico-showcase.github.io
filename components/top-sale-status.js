763a2a357cd28dfe57ba421495fecb4e30d16739
Jordi Moraleda <moraleda@uniclau.com>
2017-11-28 16:16:52 +0100

    WIP

--- components/top-sale-status.js
+++ components/top-sale-status.js
@@ -1,14 +1,113 @@
 import React from 'react';
+import { HashStore } from "../assets/contracts.js";
 
-const tokenSaleAddress = "0x123456789abcdef12345678";
+const tokenSaleAddress = "0x8af4943ED2744c229976D94045854dc5e374479a";
 
 export default class extends React.Component {
-	state = {};
+	state = {
+		status: "(Please wait)",
+		unsupported: false
+	};
 
 	componentDidMount() {
-		ethTx.connect().then(() => {
-			this.setState({connected: true});
+		this.HashStoreContract = ethTx.wrapContract(HashStore.abi, HashStore.byteCode);
+
+		ethTx.onConnectionChanged(status => this.connectionChanged(status));
+
+		this.connect().then(accounts => {
+			this.setState({ accounts });
+
+			if (accounts && accounts.length) this.setState({ status: "Web3 has been loaded" });
+			else throw new Error("Please, unlock your wallet or create an account");
+
+			return ethTx.getNetwork();
+		}).then(name => {
+			this.setState({ connected: true, network: name });
+
+			if (name != "ropsten")
+				throw new Error("Please, switch to the Ropsten network");
+
+			this.updateInterval = setInterval(() => this.updateSaleStatus(), 3000);
+			return this.updateSaleStatus();
+		}).catch(err => {
+			this.setState({ status: err.message });
 		});
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.updateInterval);
+		this.updateInterval = null;
+	}
+
+	connect() {
+		if (typeof window.web3 !== "undefined") {
+			return ethTx.useConnection(window.web3);
+		} else if (location.protocol == "file:") {
+			throw new Error("Can't connect to the Ethereum net from a local file://");
+		} else {
+			return ethTx.connect().catch(err => {
+				this.setState({ unsupported: true });
+				if (err && err.message.match(/Invalid JSON RPC response/))
+					throw new Error("You are using an unsupported browser or your connection is down");
+				else throw err;
+			});
+		}
+	}
+
+	attachToContract() {
+		if (!this.hashStoreInstance) {
+			this.hashStoreInstance = new this.HashStoreContract(tokenSaleAddress);
+		}
+	}
+
+	updateSaleStatus() {
+		this.attachToContract();
+
+		return this.hashStoreInstance
+			.getHash()
+			.call()
+			.then(hash => {
+				this.setState({ status: "Current Hash: " + hash });
+			})
+			.catch(err => {
+				alert(err.message);
+				// setStatus(err.message);
+			});
+	}
+
+	connectionChanged(status) {
+		if (!status.connected)
+			this.setState({
+				status:
+					"You are using an unsupported browser or your connection is down"
+			});
+		else if (status.accounts && status.accounts.length)
+			this.setState({ status: `Web3 connection status changed (${status.network})` });
+		else this.setState({ status: "Please, unlock your wallet or create an account" });
+	}
+
+	submit() {
+		this.attachToContract();
+debugger;
+		const hash = "0x1234";
+
+		return ethTx.getNetwork()
+			.then(name => {
+				if (name != "ropsten")
+					throw new Error("Please, switch to the Ropsten network");
+
+				return this.hashStoreInstance.setHash(hash).send({});
+			})
+			.then(result => {
+				console.log(result);
+				this.setState({status: "Updated the hash to " + hash});
+
+				return updateStatus();
+			})
+			.catch(err => {
+				alert(err.message);
+			});
+
 	}
 
 	renderWeb3Ready() {
@@ -43,7 +142,7 @@ export default class extends React.Component {
 						<button className="btn btn-outline-info" type="button"><strong>$</strong> &darr;</button>
 					</span>
 					<span className="input-group-btn">
-						<button className="btn btn-success" type="button">Fund the project</button>
+						<button className="btn btn-success" type="button" onClick={() => this.submit() }>Fund the project</button>
 					</span>
 				</div>
 				<p className="text-center text-muted">234.00 USD - 1.0234 ETH - 1234 Tokens</p>
@@ -98,7 +197,7 @@ export default class extends React.Component {
 					{this.renderWeb3Ready()}
 
 
-					<p className="text-center text-muted">{this.state.connected ? "Connected" : "Unavailable"}</p>
+					<p className="text-center text-muted">{this.state.status}</p>
 
 				</div>
 
